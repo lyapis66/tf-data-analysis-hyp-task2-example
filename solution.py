fe9e304cfc0cc0b6cb0d656d5050c617ca4e1d49
Tatiana <112729829+lyapis66@users.noreply.github.com>
2023-04-12 01:37:59 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ chat_id = 1882360450 # Ваш chat ID, не меняйте название пе
 
 def solution(x: np.array, y: np.array) -> bool:
     from scipy import stats
-    res = stats.ks_2samp(i, j)
+    res = stats.ks_2samp(x, y)
     if res.pvalue<=0.03: 
         answer=True
     else:
